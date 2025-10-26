709a700fdc61acacf261efc94c40ad9001640312
Shmelev Anton <poshtet.spb@gmail.com>
2025-10-26 17:04:47 +0300

    initialize project rksp

---
+++ src/main/java/ru/rksp/shmelev/ShmelevApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.shmelev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShmelevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShmelevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/shmelev/ShmelevApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.shmelev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShmelevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
